4b1ec242ad7fdfb41d6228d1e91367dc8944eff2
migueleuceda <miguelssj.euceda7@gmail.com>
2024-10-25 17:29:06 +0200

    JocjugableV1

--- juego/Player.gd
+++ juego/Player.gd
@@ -1,17 +1,14 @@
 extends Area2D
+
 signal hit
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
-	
+
 func _process(delta):
 	var velocity = Vector2.ZERO # Vector de moviment del jugador.
 	if Input.is_action_pressed("move_right"):
@@ -27,9 +24,11 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
@@ -37,24 +36,16 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	
 
 func _on_Player_body_entered(body):
 	hide() # El jugador desapareix després de ser impactat.
 	emit_signal("hit")
 	# S'ha d'ajornar, ja que no podem canviar les propietats físiques en una crida de retorn de física.
 	$CollisionShape2D.set_deferred("disabled", true)
-	
-	
+
+
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-
-
-
-func _on_Player_hit():
-	pass # Replace with function body.
