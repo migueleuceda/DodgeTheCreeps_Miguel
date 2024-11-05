657b7cda9b11e27cadbd7beb2c8001c1bf8e0c16
migueleuceda <miguelssj.euceda7@gmail.com>
2024-11-05 17:03:38 +0100

    Afegimsons
    agregamos sonido

--- juego/Main.gd
+++ juego/Main.gd
@@ -11,6 +11,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 
 func new_game():
@@ -20,6 +22,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_ScoreTimer_timeout():
 	score += 1
